7b56a4c714e4f1ec62b0f6536e70b077ba463052
Luciano Ramalho <luciano@ramalho.org>
2015-05-14 02:06:32 -0300

    Update clock.py

--- lcdserial/clock.py
+++ lcdserial/clock.py
@@ -3,7 +3,7 @@ import time
 from pingo.parts import serial
 
 # '/dev/ttyAMA0' is the mini-UART port on the Raspberry Pi
-lcd = serial.LCD16x2('/dev/ttyAMA0', 9600)
+lcd = serial.LCD16x2('/dev/ttyAMA0')
 time.sleep(.5)  # wait for display to boot up
 
 while True:
