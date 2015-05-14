963ffca37e8cf0ba50119fa0dff774299081638d
Luciano Ramalho <luciano@ramalho.org>
2015-05-14 02:51:18 -0300

    added command-line option to set serial port

--- lcdserial/clock.py
+++ lcdserial/clock.py
@@ -1,13 +1,19 @@
 import time
+import sys
 
 from pingo.parts import serial
 
-# '/dev/ttyAMA0' is the mini-UART port on the Raspberry Pi
-lcd = serial.LCD16x2('/dev/ttyAMA0')
-time.sleep(.5)  # wait for display to boot up
-lcd.clear()
+DEFAULT_PORT = '/dev/ttyAMA0'  # the mini-UART port on the Raspberry Pi
 
-while True:
-    lcd.set_cursor(0, 0)
-    lcd.write(time.strftime('%H:%M:%S'))
-    time.sleep(1)
+def main(port=DEFAULT_PORT):
+    lcd = serial.LCD16x2(port)
+    time.sleep(.5)  # wait for display to boot up
+    lcd.clear()
+
+    while True:
+        lcd.set_cursor(0, 0)
+        lcd.write(time.strftime('%H:%M:%S'))
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
