5992edd0b1d0bbd07d1b4710f90605d2ae480687
Luciano Ramalho <luciano@ramalho.org>
2015-05-14 03:48:10 -0300

    Update displaymsg.py

--- lcdserial/displaymsg.py
+++ lcdserial/displaymsg.py
@@ -1,4 +1,5 @@
 import sys
+import collections
 
 from pingo.parts import serial
 
@@ -7,10 +8,12 @@ DEFAULT_PORT = '/dev/ttyAMA0'  # the mini-UART port on the Raspberry Pi
 def main(port=DEFAULT_PORT):
     lcd = serial.LCD16x2(port)
     lcd.clear()
+    lines = collections.deque(['',''], 2)
     while True:
-        msg = raw_input('> ')
-        lcd.set_cursor(0, 0)
-        lcd.write(msg)
+        lines.append(raw_input('> '))
+        for i, line in enumerate(lines):
+            lcd.set_cursor(i, 0)
+            lcd.write(line)
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
