6d45f82c9b4395a0ba390591735f30b2408c4062
Luciano Ramalho <luciano@ramalho.org>
2015-05-14 02:03:14 -0300

    Update clock.py

--- lcdserial/clock.py
+++ lcdserial/clock.py
@@ -6,6 +6,6 @@ from pingo.parts import serial
 lcd = serial.LCD16x2('/dev/ttyAMA0', 9600)
 
 while True:
-    lcd.move_cursor(0, 0)
+    lcd.set_cursor(0, 0)
     lcd.write(time.strftime('%H:%M:%S'))
     time.sleep(1)
