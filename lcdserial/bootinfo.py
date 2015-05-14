e57687b0240203c51ad4c41c4ac4312c88f3aa24
Luciano Ramalho <luciano@ramalho.org>
2015-05-14 20:54:20 -0300

    adapted bootinfo to deal with more than one IP address

--- lcdserial/bootinfo.py
+++ lcdserial/bootinfo.py
@@ -7,12 +7,15 @@ from pingo.parts import serial
 
 DEFAULT_PORT = '/dev/ttyAMA0'  # the mini-UART port on the Raspberry Pi
 
-def main(msgs):
+def main(*msgs):
     lcd = serial.LCD16x2(DEFAULT_PORT)
     lcd.clear()
-    for i, msg in enumerate(msgs[:2]):
-        lcd.set_cursor(i, 0)
+    for i, msg in enumerate(msgs):
+        lcd.set_cursor(i%2, 0)
         lcd.write(msg)
+        if i > 1:
+            time.sleep(.5)
+
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
