042debc3bbed6273ab385ac802e2bdc5df84a349
Luciano Ramalho <luciano@ramalho.org>
2015-05-14 03:42:52 -0300

    Update displaymsg.py

--- lcdserial/displaymsg.py
+++ lcdserial/displaymsg.py
@@ -6,10 +6,10 @@ DEFAULT_PORT = '/dev/ttyAMA0'  # the mini-UART port on the Raspberry Pi
 
 def main(port=DEFAULT_PORT):
     lcd = serial.LCD16x2(port)
+    lcd.clear()
     while True:
-        lcd.clear()
-        lcd.set_cursor(0, 0)
         msg = raw_input('> ')
+        lcd.set_cursor(0, 0)
         lcd.write(msg)
 
 if __name__ == '__main__':
