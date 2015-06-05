d57a91faceaf39fec40162862342739176875206
Luciano Ramalho <luciano@ramalho.org>
2015-06-04 23:15:29 -0300

    demos RGBLed part

---
+++ rpi/rgb6colors.py
@@ -0,0 +1,15 @@
+import pingo
+
+board = pingo.detect.get_board()
+
+led_pins = [board.pins[i] for i in [24, 23, 21]]
+
+for pin in led_pins:
+    pin.mode = pingo.OUT
+
+rgb = pingo.parts.led.RGBLed(*led_pins)
+
+while True:
+    for color, _ in pingo.parts.led.PURE_COLORS:
+        rgb.set_color(color)
+        raw_input(color)
--- rpi/rgb_cycle.py
+++ rpi/rgb_cycle.py
@@ -2,8 +2,8 @@ import pingo
 
 board = pingo.detect.get_board()
 
-led_pins = [24, 21, 23]
-led_pins = [board.pins[i] for i in led_pins]
+led_pins = [board.pins[i] for i in [24, 23, 21]]
+
 for pin in led_pins:
     pin.mode = pingo.OUT
 
